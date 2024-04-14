f24f5fbcd32bd754453a0b4ba79d62fbb6457188
Barracuda72 <barracuda72@bk.ru>
2024-04-14 23:54:35 +0500

    Enable OpenGL debug

--- src/PEngine/app.cpp
+++ src/PEngine/app.cpp
@@ -38,6 +38,25 @@ suspect SDL does it for you on quit. Am I right?
 */
 
 
+void GLAPIENTRY
+MessageCallback( GLenum source,
+                 GLenum type,
+                 GLuint id,
+                 GLenum severity,
+                 GLsizei length,
+                 const GLchar* message,
+                 const void* userParam )
+{
+  (void)source;
+  (void)id;
+  (void)length;
+  (void)userParam;
+
+  fprintf( stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+           ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ),
+            type, severity, message );
+}
+
 int PUtil::deblev = DEBUGLEVEL_ENDUSER;
 
 
@@ -404,6 +423,9 @@ int PApp::run(int argc, char *argv[])
     glGetString(GL_VENDOR) << " " << glGetString(GL_RENDERER) << std::endl;
 
   PUtil::outLog() << "Using OpenGL " << glGetString(GL_VERSION) << std::endl;
+
+  glEnable(GL_DEBUG_OUTPUT);
+  glDebugMessageCallback(MessageCallback, 0);
 
   switch (stereo) {
   default: break;
