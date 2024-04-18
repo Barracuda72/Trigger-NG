742498ba5215825816eb373119ba6f21c143926c
Barracuda72 <barracuda72@bk.ru>
2024-04-18 13:12:32 +0500

    Fix vertex shader for sky

--- data/shaders/sky_vsh.glsl
+++ data/shaders/sky_vsh.glsl
@@ -7,6 +7,6 @@ uniform mat4 t_transform;
 
 void main()
 {
-    gl_Position = gl_ModelViewMatrix * vec4(vert_coord, 1.0);
     tex_transformed = (t_transform * vec4(tex_coord, 0.0, 1.0)).xy;
+    gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * vec4(vert_coord, 1.0);
 }
